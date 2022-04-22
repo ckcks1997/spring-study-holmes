1319f8b16d6deb6e32baf376f1b6b5e00b257b24
kkezi <tjgggg4@naver.com>
2022-04-22 10:37:12 +0900

    - notice 빈 추가

--- src/main/java/service/NoticeDao.java
+++ src/main/java/service/NoticeDao.java
@@ -4,68 +4,72 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import model.Notice;
 import util.MybatisConnection;
 
+@Component
 public class NoticeDao {
 
   private static final String NS = "notice.";
   private Map<String, Object> map = new HashMap<>();
+  
+  @Autowired
+	MySqlSessionFactory sqlSessionFactory;
+	SqlSession sqlSession;
+	
+	@PostConstruct
+	public void setSqlSession() {
+		this.sqlSession = sqlSessionFactory.sqlmap.openSession();
+	}
+			
+	
+  
+  
 
   public int noticeNew(String id) {
 
-    SqlSession sqlSession = MybatisConnection.getConnection();
-    try {
+    
       return sqlSession.selectOne(NS + "noticeNew", id);
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      MybatisConnection.close(sqlSession);
-    }
-    return 0;
+    
   }
-
+  
+  
   public List<Notice> noticeGet(String id) {
 
-    SqlSession sqlSession = MybatisConnection.getConnection();
-    try {
+    
+    
       return sqlSession.selectList(NS + "noticeGet", id);
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      MybatisConnection.close(sqlSession);
-    }
-    return null;
+    
   }
 
   public int noticeRead(String id) {
-    SqlSession sqlSession = MybatisConnection.getConnection();
+    
     try {
       return sqlSession.update(NS + "noticeRead", id);
     } catch (Exception e) {
       e.printStackTrace();
     } finally {
-      MybatisConnection.close(sqlSession);
+      sqlSession.commit();
     }
     return 0;
   }
 
   public int noticeWrite(int boardid, String from, String to) {
-    SqlSession sqlSession = MybatisConnection.getConnection();
-    try {
+    
+
       map.clear();
       map.put("boardid", boardid);
       map.put("from", from);
       map.put("to", to);
       
       return sqlSession.insert(NS + "noticeWrite", map);
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      MybatisConnection.close(sqlSession);
-    }
-    return 0;
+    
   }
   
   
@@ -73,8 +77,8 @@ public class NoticeDao {
   
   
   public int noticeReportWrite(String info, String from, String to, int board_num) {
-	    SqlSession sqlSession = MybatisConnection.getConnection();
-	    try {
+	   
+	    
 	      map.clear();
 	      map.put("info", info);
 	      map.put("from", from);
@@ -82,12 +86,7 @@ public class NoticeDao {
 	      map.put("board_num", board_num);
 	      
 	      return sqlSession.insert(NS + "noticeReportWrite", map);
-	    } catch (Exception e) {
-	      e.printStackTrace();
-	    } finally {
-	      MybatisConnection.close(sqlSession);
-	    }
-	    return 0;
+	   
 	  }
   
   
@@ -98,15 +97,8 @@ public class NoticeDao {
   
   public Notice noticeGetByNoticeNum(int noticeNum) {
 
-    SqlSession sqlSession = MybatisConnection.getConnection();
-    try {
       return sqlSession.selectOne(NS + "noticeGetByNoticeNum", noticeNum);
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      MybatisConnection.close(sqlSession);
-    }
-    return null;
+   
   }
 
 }
