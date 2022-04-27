354ed395116a18eb0f692469fb0b62a26e07aeda
ckcks1997 <ckcks1997@gmail.com>
2022-04-27 23:14:39 +0900

    -카카오 로그인 REST API 방식 수정 -서블릿 컨텍스트 공백 정리

---
+++ src/main/java/service/KakaoService.java
@@ -0,0 +1,107 @@
+package service;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+import org.springframework.stereotype.Service;
+ 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+ 
+@Service
+public class KakaoService {
+	// 아래 블로그 내용을 참고하여 구현하였습니다.
+	// https://mylupin.tistory.com/48?category=792579
+
+        public String getAccessToken (String authorize_code) {
+            String access_Token = "";
+            String refresh_Token = "";
+            String reqURL = "https://kauth.kakao.com/oauth/token";
+
+            try {
+                URL url = new URL(reqURL);
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                conn.setDoOutput(true);
+                conn.setRequestMethod("POST");
+ 
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+                StringBuilder sb = new StringBuilder();
+                sb.append("grant_type=authorization_code");
+                sb.append("&client_id=6fe656d9f07e9bc602d5ed7ec6cb3c20");  //본인이 발급받은 key
+                sb.append("&redirect_uri=http://localhost:8080/spring-study-homles/studymember/kakaologin");     // 본인이 설정해 놓은 경로
+                sb.append("&code=" + authorize_code);
+                bw.write(sb.toString());
+                bw.flush();
+
+                //    요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line = "";
+                String result = "";
+
+                while ((line = br.readLine()) != null) {
+                    result += line;
+                }
+
+                JsonElement element = JsonParser.parseString(result);
+
+                access_Token = element.getAsJsonObject().get("access_token").getAsString();
+                refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
+
+                br.close();
+                bw.close();
+            } catch (IOException e) { 
+                e.printStackTrace();
+            }
+
+            return access_Token;
+        }
+        
+        
+        public HashMap<String, Object> getUserInfo (String access_Token) {
+
+            HashMap<String, Object> userInfo = new HashMap<>();
+            String reqURL = "https://kapi.kakao.com/v2/user/me";
+            try {
+                URL url = new URL(reqURL);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                conn.setDoOutput(true);
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+                
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                String line = "";
+                String result = "";
+
+                while ((line = br.readLine()) != null) {
+                    result += line;
+                } 
+ 
+                JsonElement element = JsonParser.parseString(result);
+
+                JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+                JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+
+                String nickname = properties.getAsJsonObject().get("nickname").getAsString();
+                String email = kakao_account.getAsJsonObject().get("email").getAsString();
+
+                userInfo.put("nickname", nickname);
+                userInfo.put("email", email);
+
+            } catch (IOException e) { 
+                e.printStackTrace();
+            }
+
+            return userInfo;
+        }
+}
